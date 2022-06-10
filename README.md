be965cd32154fb1ca845cb5950cf774e12c78d27
Isaac <swengineerwithisaac@gmail.com>
2022-06-10 17:51:23 +0800

    Updated app.js

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## Weather app from API call
 
+Link to the weather API: https://openweathermap.org/city/1733046
+be sure read their documentation and get started!.
--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 
 const express = require("express");
 const https = require("https");
+const bodyData = require("body-parser");
 const app = express();
+
+app.use(bodyData.urlencoded({
+    extended: true
+}));
 
 app.listen(3000, () => {
     console.log("Server has started on port 3000");
 });
 
 app.get("/", (req, res) => {
-
-    const API_URL = "https://api.openweathermap.org/data/2.5/weather?id=524901&appid=30aafa44186f5691dfbf24a44ed73b94&units=metric&q=Malaysia"
+    res.sendFile(__dirname + "/index.html");
+});
+app.post("/", (req, res) => { 
+    // Get the POST req when user click view
+    const key = "30aafa44186f5691dfbf24a44ed73b94";
+    const country = req.body.countryName; // Store the user input
+    const API_URL = `https://api.openweathermap.org/data/2.5/weather?id=524901&appid=${key}&units=metric&q=${country}`
     // URL authentication and parameters using POSTMAN
-
+    
     https.get(API_URL, (response) => {
         console.log(response.statusCode);
         
@@ -24,15 +34,15 @@ app.get("/", (req, res) => {
             const description = weatherData.weather[0].description; // Get the des
             const icon = weatherData.weather[0].icon;
             const iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-
+    
             res.write(`
-                    <h1>The temperature in Malaysia its ${temp} degree Celsius </h1>
+                    <h1>The temperature in ${country} its ${temp} degree Celsius </h1>
                     <p>The weather its currently ${description}</p>
                     <img src="${iconURL}" alt="An image of the current weather" >
                 `);
-
+    
             res.send(); // After write we send the string
             console.log(temp);
         });
     });
-});
+})
